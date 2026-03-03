5ed6072bacd3aa1ae763596e0e6d658d0c137c08
Demetrio Marra <demetrio.marra3@gmail.com>
2026-03-03 15:31:39 +0100

    Add IApiDocumentationService interface for JS API docs
    Introduced the IApiDocumentationService interface in the AgentMesh.Services namespace. This interface provides an async method to retrieve technical JavaScript documentation for a specified API. XML documentation comments were added for clarity.

---
+++ AgentMesh/Services/IApiDocumentationService.cs
@@ -0,0 +1,15 @@
+﻿namespace AgentMesh.Services
+{
+    /// <summary>
+    /// Provides functionality for managing and accessing API technical Javascript documentation.
+    /// </summary>
+    public interface IApiDocumentationService
+    {
+            /// <summary>
+            /// Retrieves the technical Javascript documentation for a specified API.
+            /// </summary>
+            /// <param name="apiName">The name of the API for which to retrieve documentation.</param>
+            /// <returns>A string containing the technical Javascript documentation for the specified API.</returns>
+            Task<string> GetApiDocumentationAsync(string apiName);
+    }
+}
