4add02e6c439d836d6571564d59c269aab66f23e
Demetrio Marra <demetrio.marra3@gmail.com>
2026-03-06 18:37:55 +0100

    Add agent memory models and executor interface
    Introduced models for memory interaction input, search input, and search output. Added IAgentMemoryExecutor interface with async methods for searching memory and adding interactions. These changes establish a foundation for context-aware agent memory management in AgentMesh.

---
+++ AgentMesh/Services/IAgentMemoryExecutor.cs
@@ -0,0 +1,18 @@
+﻿using AgentMesh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentMesh.Services
+{
+    public interface IAgentMemoryExecutor
+    {
+
+        Task<AgentMemoryExecutorOutput> SearchMemoryAsync(AgentMemoryExecutorSearchMemoryInput input);
+
+
+        Task AddInteractionAsync(AgentMemoryExecutorAddInteractionInput input);
+    }
+}
