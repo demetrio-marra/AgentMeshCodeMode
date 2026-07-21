86225cfda3d14a8bd4c87cefe9748227e3bb8df7
Demetrio Marra <demetrio.marra3@gmail.com>
2026-07-21 19:05:13 +0200

    added new IWorkflowStep interface

---
+++ AgentMesh/Models/Workflows/IWorkflowStep.cs
@@ -0,0 +1,7 @@
+﻿namespace AgentMesh.Models.Workflows
+{
+    public interface IWorkflowStep<T>
+    {
+        Task<WorkflowStepUsageEntry> ExecuteAsync(T stateObject, CancellationToken cancellationToken = default);
+    }
+}
