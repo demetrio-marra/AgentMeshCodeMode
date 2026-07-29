c1e88b53d23e01bcc0c44f475f6a17fd63ff6da1
Demetrio Marra <demetrio.marra3@gmail.com>
2026-07-29 08:44:31 +0200

    add generic serialization utils

---
+++ AgentMesh/Utils/SerializationUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace AgentMesh.Utils
+{
+    public static class SerializationUtils
+    {
+        public static readonly JsonSerializerOptions DefaultSerializeOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+            Converters =
+            {
+                new System.Text.Json.Serialization.JsonStringEnumConverter()
+            },
+            IgnoreReadOnlyProperties = true
+        };
+
+        public static readonly JsonSerializerOptions DefaultDeserializeOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            Converters =
+            {
+                new System.Text.Json.Serialization.JsonStringEnumConverter()
+            }
+        };
+    }
+}
