b5cc2d9d8d32433bb3db2b6f8c27405d57cb16e4
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-07 16:49:26 +0200

    removed parameters factory

--- AgentMesh/Services/IEWParametersFactory.cs
+++
@@ -1,9 +0,0 @@
-using AgentMesh.Models.Workflows;
-
-namespace AgentMesh.Services
-{
-    public interface IEWParametersFactory
-    {
-        IEnumerable<IEWParameter> CreateParameters();
-    }
-}
