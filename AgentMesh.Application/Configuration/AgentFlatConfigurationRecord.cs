8f96145e64994a7785bb2c6c2d51d69e93919c21
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-09 14:35:48 +0200

    added agent generic configuration

---
+++ AgentMesh.Application/Configuration/AgentFlatConfigurationRecord.cs
@@ -0,0 +1,19 @@
+﻿namespace AgentMesh.Application.Configuration
+{
+    public readonly record struct AgentFlatConfigurationRecord
+    {
+        public readonly string AgentUniqueRole { get; init; }
+
+        public readonly string ProviderName { get; init; }
+        public readonly string ProviderEndpoint { get; init; }
+        public readonly string ProviderApiKey { get; init; }
+        public readonly string ProviderModelName { get; init; }
+
+        public readonly string LLMClass { get; init; }
+        public readonly double LLMClassCostPerMillionInputTokens { get; init; }
+        public readonly double LLMClassCostPerMillionOutputTokens { get; init; }
+        
+        public readonly string Temperature { get; init; }
+        public readonly string SystemPrompt { get; init; }
+    }
+}
