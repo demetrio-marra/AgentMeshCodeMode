e30c8d3737ea1fdb7b1bb82e384de30b942222cd
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-10 16:10:02 +0200

    migrated DocumentationEWAgenticStep

--- AgentMesh.Application/Services/EWSteps/DocumentationEWAgenticStep.cs
+++ AgentMesh.Application/Services/EWSteps/DocumentationEWAgenticStep.cs
@@ -1,11 +1,7 @@
-using AgentMesh.Application.Models.Documentation;
-using AgentMesh.Application.Models.RequestAnalysis;
 using AgentMesh.Application.Models.Workflows.Parameters;
 using AgentMesh.Application.Services.Agents;
 using AgentMesh.Models.Workflows;
 using AgentMesh.Services;
-using AgentMesh.Utils;
-using System.Text.Json;
 
 namespace AgentMesh.Application.Services.EWSteps
 {
@@ -28,21 +24,6 @@ namespace AgentMesh.Application.Services.EWSteps
 
         public async Task<EWStepResultRecord> ExecuteAsync(CancellationToken cancellationToken = default)
         {
-            var sr = new StructuredUserRequest
-            {
-                Intent = userIntentParameter.ParameterValue ?? string.Empty
-            };
-
-            var kbContent = JsonSerializer.Serialize(domainsKnowledgeBaseDocumentsContentParameter.ParameterValue ?? [], SerializationUtils.DefaultSerializeOptions);
-
-            var agentInput = new DocumentationAgentInput
-            {
-                UserRequest = sr,
-                AgentMemories = (pastMemoriesQueryResultsParameter.ParameterValue ?? []).Select(m => m.Memory),
-                KnowledgeBaseDocumentsContent = kbContent,
-                LanguageOfTheUser = languageOfTheUserParameter.ParameterValue ?? string.Empty
-            };
-
             var agentOutput = await documentationAgent.ExecuteAsync([
                 requestDateTimeParameter,
                 userIntentParameter,
