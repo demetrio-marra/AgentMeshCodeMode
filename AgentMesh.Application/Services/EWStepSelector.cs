f480163ee342a8fc8f57f217e64a77dddf1b70e8
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-12 13:37:18 +0200

    Step selector logic simplified in Other task pipeline

--- AgentMesh.Application/Services/EWStepSelector.cs
+++ AgentMesh.Application/Services/EWStepSelector.cs
@@ -7,6 +7,43 @@ using AgentMesh.Services;
 
 namespace AgentMesh.Application.Services
 {
+    /// <summary>
+    /// To work this instance must be scoped
+    /// </summary>
+    /// <param name="initEWCodeStep"></param>
+    /// <param name="intentCategoryParameter"></param>
+    /// <param name="missingValuesParameter"></param>
+    /// <param name="knowledgeBaseAPIDocumentsContentParameter"></param>
+    /// <param name="pastMemoriesQueryParameter"></param>
+    /// <param name="domainsKnowledgeBaseQueryParameter"></param>
+    /// <param name="pastMemoriesQueryResultsParameter"></param>
+    /// <param name="knowledgeBaseQueryResultsParameter"></param>
+    /// <param name="domainsKnowledgeBaseDocumentsContentParameter"></param>
+    /// <param name="businessRequirementsParameter"></param>
+    /// <param name="technicalSpecificationParameter"></param>
+    /// <param name="requestRejectedReasonParameter"></param>
+    /// <param name="apisKnowledgeBaseQueryResultsParameter"></param>
+    /// <param name="generatedCodeParameter"></param>
+    /// <param name="pipelineResultDataParameter"></param>
+    /// <param name="executionErrorParameter"></param>
+    /// <param name="finalAnswerParameter"></param>
+    /// <param name="workflowConfiguration"></param>
+    /// <param name="requestAnalyzerEWStep"></param>
+    /// <param name="agentMemoryQueryExpanderEWStep"></param>
+    /// <param name="agentMemoryServiceEWStep"></param>
+    /// <param name="knowledgeBaseQueryExpanderEWStep"></param>
+    /// <param name="domainsKnowledgeBaseServiceSearchEWStep"></param>
+    /// <param name="rerankerEWStep"></param>
+    /// <param name="domainsKnowledgeBaseDocumentsExtractorEWStep"></param>
+    /// <param name="documentationEWStep"></param>
+    /// <param name="functionalAnalystEWStep"></param>
+    /// <param name="apisKnowledgeBaseServiceSearchEWStep"></param>
+    /// <param name="apiKnowledgeBaseDocumentsExtractorEWStep"></param>
+    /// <param name="technicalAnalystEWStep"></param>
+    /// <param name="coderEWStep"></param>
+    /// <param name="jsSandboxEWStep"></param>
+    /// <param name="domainExpertEWStep"></param>
+    /// <param name="personalAssistantEWStep"></param>
     public class EWStepSelector(
         InitEWCodeStep initEWCodeStep,
 
@@ -53,41 +90,51 @@ namespace AgentMesh.Application.Services
         private bool _rerankerHasRun = false;
         private bool _domainExpertHasRun = false;
 
+
         public IEnumerable<IEWStep> NextStepsToRun()
         {
+            if (RunOnce([requestAnalyzerEWStep], out var steps))
+            {
+                return steps;
+            }
+
             var pipelineBranch = GuessPipelineBranch();
             return pipelineBranch switch
             {
                 PipelineBranchValue.OtherTopics => HandleOtherTopicsBranch(),
                 PipelineBranchValue.Documenting => HandleDocumentingBranch(),
                 PipelineBranchValue.TaskExecution => HandleTaskExecutionBranch(),
-                PipelineBranchValue.None => [requestAnalyzerEWStep],
-                _ => [requestAnalyzerEWStep],
+                _ => throw new NotImplementedException()
             };
         }
 
+
         private IEnumerable<IEWStep> HandleOtherTopicsBranch()
         {
-            if (pastMemoriesQueryParameter.GetDisplayValue() == EWParameterConstants.NoDataPlaceholder
-               && missingValuesParameter.GetDisplayValue() != EWParameterConstants.NoDataPlaceholder)
+            var steps = Enumerable.Empty<IEWStep>();
+
+            // Equivalent to:
+            // - if agent has already run, do not run it
+            // - if missingValuesParameter is null or empty, do not run it
+            // Conditions are AND
+            if (RunOnce([agentMemoryQueryExpanderEWStep], out steps,
+                () => missingValuesParameter.ParameterValue?.Any() ?? false))
             {
-                return [agentMemoryQueryExpanderEWStep];
+                return steps;
+            }
+           
+            if (RunOnce([agentMemoryServiceEWStep], out steps,
+                () => pastMemoriesQueryParameter.ParameterValue?.Any() ?? false))
+            {
+                return steps;
             }
 
-            if (pastMemoriesQueryResultsParameter.GetDisplayValue() == EWParameterConstants.NoDataPlaceholder
-                && pastMemoriesQueryParameter.GetDisplayValue() != EWParameterConstants.NoDataPlaceholder)
+            if (RunOnce([personalAssistantEWStep], out steps))
             {
-                return [agentMemoryServiceEWStep];
+                return steps;
             }
 
-            if (finalAnswerParameter.GetDisplayValue() == EWParameterConstants.NoDataPlaceholder)
-            {
-                return [personalAssistantEWStep];
-            }
-            else
-            {
-                return [];
-            }
+            return steps;
         }
 
         private IEnumerable<IEWStep> HandleDocumentingBranch()
@@ -97,7 +144,7 @@ namespace AgentMesh.Application.Services
             {
                 return [agentMemoryQueryExpanderEWStep];
             }
-            
+
             if (pastMemoriesQueryResultsParameter.GetDisplayValue() == EWParameterConstants.NoDataPlaceholder
                 && pastMemoriesQueryParameter.GetDisplayValue() != EWParameterConstants.NoDataPlaceholder)
             {
@@ -127,7 +174,7 @@ namespace AgentMesh.Application.Services
                 return [domainsKnowledgeBaseDocumentsExtractorEWStep];
             }
 
-            if (pipelineResultDataParameter.GetDisplayValue() == EWParameterConstants.NoDataPlaceholder)                
+            if (pipelineResultDataParameter.GetDisplayValue() == EWParameterConstants.NoDataPlaceholder)
             {
                 return [documentationEWStep];
             }
@@ -141,7 +188,7 @@ namespace AgentMesh.Application.Services
                 return [];
             }
         }
-        
+
         private IEnumerable<IEWStep> HandleTaskExecutionBranch()
         {
             if (pastMemoriesQueryParameter.GetDisplayValue() == EWParameterConstants.NoDataPlaceholder
@@ -235,28 +282,46 @@ namespace AgentMesh.Application.Services
 
         private PipelineBranchValue GuessPipelineBranch()
         {
-            if (!intentCategoryParameter.ParameterValue.HasValue)
+            return intentCategoryParameter.ParameterValue!.Value switch
             {
-                return PipelineBranchValue.None;
-            }
-            else
-            {
-                return intentCategoryParameter.ParameterValue.Value switch
-                {
-                    UserIntentCategory.Other => PipelineBranchValue.OtherTopics,
-                    UserIntentCategory.Documentation => PipelineBranchValue.Documenting,
-                    UserIntentCategory.TaskExecution => PipelineBranchValue.TaskExecution,
-                    _ => PipelineBranchValue.None
-                };
-            }
+                UserIntentCategory.Other => PipelineBranchValue.OtherTopics,
+                UserIntentCategory.Documentation => PipelineBranchValue.Documenting,
+                UserIntentCategory.TaskExecution => PipelineBranchValue.TaskExecution,
+            };
         }
 
         private enum PipelineBranchValue
         {
-            None,
             OtherTopics,
             Documenting,
             TaskExecution
+        }
+
+        private readonly Dictionary<string, bool> _runOnceDictionary = [];
+        private bool RunOnce(IEnumerable<IEWStep> steps,
+            out IEnumerable<IEWStep> stepsToRun,
+            Func<bool>? runCondition = null)
+        {
+            stepsToRun = [];
+            
+            if (runCondition != null
+                && !runCondition())
+            {
+                return false;
+            }
+
+            var ls = new List<IEWStep>();
+            foreach (var step in steps)
+            {
+                if (!_runOnceDictionary.TryGetValue(step.Name, out bool hasRun) || !hasRun)
+                {
+                    ls.Add(step);
+                    _runOnceDictionary[step.Name] = true;
+                }
+            }
+            stepsToRun = ls;
+
+            return ls.Any();
         }
     }
 }
