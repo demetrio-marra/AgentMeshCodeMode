28a8ecfbd36596ee02ada0f61681977a0d767a0c
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-17 10:58:21 +0200

    implmenented ctrl+c

--- AgentMeshCLI/Services/UserConsoleInputService.cs
+++ AgentMeshCLI/Services/UserConsoleInputService.cs
@@ -1,5 +1,5 @@
-using AgentMesh.Application.Services;
 using AgentMesh.Application.Configuration;
+using AgentMesh.Application.Services;
 using AgentMesh.Helpers;
 using AgentMesh.Infrastructure.JSSandbox;
 using Microsoft.Extensions.Hosting;
@@ -16,11 +16,13 @@ namespace AgentMesh.Services
     {
         public async Task Run(CancellationToken cancellationToken)
         {
+            bool isFirstRun = true;
+
             Console.WriteLine("Welcome to AgentMesh! This is a console application that allows you to interact with the AgentMesh system.\n");
 
             PrintConfigurations();
 
-            while (true)
+            while (!cancellationToken.IsCancellationRequested)
             {
                 Console.WriteLine("Enter your question or type /help:");
                 Console.Write("> ");
@@ -50,30 +52,81 @@ namespace AgentMesh.Services
                     continue;
                 }
 
-                var executionResult = await appInstance.ProcessRequest(requestText!, cancellationToken);
-
-                ConsoleHelper.WriteLineWithColor("\nResponse for user:", ConsoleColor.Gray);
-                ConsoleHelper.WriteLineWithColor(executionResult.Message, ConsoleColor.Cyan);
-
-                ConsoleHelper.WriteLineWithColor($"\n\nConversation status:\nCount of messages {executionResult.CountOfMessages}\nCount of tokens: {executionResult.CountOfTokens}/{conversationSummarizerConfiguration.SummaryTokenThreshold}\nCumulated cost: {Math.Round(executionResult.CumulatedCost, 2)} $", ConsoleColor.Gray);
-                if (executionResult.ContextSummarizerHasRun)
+                if (isFirstRun)
                 {
-                    ConsoleHelper.WriteLineWithColor($"Chat conversation has been summarized. Count of messages before: {executionResult.CountOfMessagesBeforeSummarization}", ConsoleColor.White);
-
+                    ConsoleHelper.WriteLineWithColor("You can cancel the current request by pressing Ctrl+C.\n", ConsoleColor.Yellow);
+                    isFirstRun = false;
                 }
 
-                ConsoleHelper.PrintTokenUsageSummary(executionResult.MainPipelineStepsData,
-                    executionResult.AgentsCostData);
+                var previousTreatControlCAsInput = Console.TreatControlCAsInput;
+                Console.TreatControlCAsInput = true;
+
+                using var requestCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                var cancelMonitorTask = MonitorRequestCancellationByKeyboardAsync(requestCancellationTokenSource, cancellationToken);
+
+                try
+                {
+                    var executionResult = await appInstance.ProcessRequest(requestText!, requestCancellationTokenSource.Token);
+
+                    ConsoleHelper.WriteLineWithColor("\nResponse for user:", ConsoleColor.Gray);
+                    ConsoleHelper.WriteLineWithColor(executionResult.Message, ConsoleColor.Cyan);
+
+                    ConsoleHelper.WriteLineWithColor($"\n\nConversation status:\nCount of messages {executionResult.CountOfMessages}\nCount of tokens: {executionResult.CountOfTokens}/{conversationSummarizerConfiguration.SummaryTokenThreshold}\nCumulated cost: {Math.Round(executionResult.CumulatedCost, 2)} $", ConsoleColor.Gray);
+                    if (executionResult.ContextSummarizerHasRun)
+                    {
+                        ConsoleHelper.WriteLineWithColor($"Chat conversation has been summarized. Count of messages before: {executionResult.CountOfMessagesBeforeSummarization}", ConsoleColor.White);
+
+                    }
+
+                    ConsoleHelper.PrintTokenUsageSummary(executionResult.MainPipelineStepsData,
+                        executionResult.AgentsCostData);
+                }
+                catch (OperationCanceledException) when (requestCancellationTokenSource.IsCancellationRequested && !cancellationToken.IsCancellationRequested)
+                {
+                    ConsoleHelper.WriteLineWithColor("Request canceled.", ConsoleColor.Yellow);
+                }
+                finally
+                {
+                    requestCancellationTokenSource.Cancel();
+                    await cancelMonitorTask;
+                    Console.TreatControlCAsInput = previousTreatControlCAsInput;
+                }
             }
         }
-     
+
+        private static async Task MonitorRequestCancellationByKeyboardAsync(CancellationTokenSource requestCancellationTokenSource, CancellationToken appCancellationToken)
+        {
+            while (!requestCancellationTokenSource.IsCancellationRequested && !appCancellationToken.IsCancellationRequested)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var keyInfo = Console.ReadKey(intercept: true);
+                    if (keyInfo.Key == ConsoleKey.C && keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control))
+                    {
+                        requestCancellationTokenSource.Cancel();
+                        ConsoleHelper.WriteLineWithColor("\nCurrent request cancellation requested...", ConsoleColor.Yellow);
+                        return;
+                    }
+                }
+
+                try
+                {
+                    await Task.Delay(50, appCancellationToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    return;
+                }
+            }
+        }
+
 
         private void PrintConfigurations()
         {
             Console.WriteLine($"Sandbox:\n\tUrl: {sesJSSandboxConfiguration.SandboxServiceURL}\n\tName: {sesJSSandboxConfiguration.SandboxName}\n\tAgentId: {userConfiguration.AgentId}\n");
             Console.WriteLine($"Conversation summarization configuration:\n\tSummaryTokenThreshold: {conversationSummarizerConfiguration.SummaryTokenThreshold}\n\tNumMessageToPreseve: {conversationSummarizerConfiguration.NumMessageToPreseve}\n");
             Console.WriteLine("Agent configurations:");
-            foreach(var agentConfig in agentsConfigurations)
+            foreach (var agentConfig in agentsConfigurations)
             {
                 ConsoleHelper.PrintAgentConfiguration(agentConfig.AgentUniqueRole, agentConfig.ProviderModelName, Convert.ToDouble(agentConfig.Temperature, CultureInfo.InvariantCulture));
             }
@@ -86,6 +139,7 @@ namespace AgentMesh.Services
             Console.WriteLine("/help - Show this help message");
             Console.WriteLine("/exit - Exit the application");
             Console.WriteLine("/new - Initializes a new conversation");
+            Console.WriteLine("Ctrl+C - Cancel the current request");
             Console.WriteLine("Any other text will be treated as a question to the AgentMesh system.\n");
         }
 
