ad5be2a6a592ecadd989aac5a81b765c4d0d501e
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-27 21:09:09 +0200

    added KnowledgeQueryResult

---
+++ AgentMesh.Application/Models/Knowledge/KnowledgeQueryResult.cs
@@ -0,0 +1,9 @@
+﻿namespace AgentMesh.Application.Models.Knowledge
+{
+    public class KnowledgeQueryResult
+    {
+        public IEnumerable<KnowledgeContentItem> Contents { get; set; } = [];
+        public IEnumerable<KnowledgeEntityItem> Entities { get; set; } = [];
+        public IEnumerable<KnowledgeRelationItem> Relations { get; set; } = [];
+    }
+}
