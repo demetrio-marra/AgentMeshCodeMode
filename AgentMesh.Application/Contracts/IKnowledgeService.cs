cef81100d21e115d9fe0f3dd73002900c175bf10
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-28 09:15:14 +0200

    add KnowledgeService and query models

---
+++ AgentMesh.Application/Contracts/IKnowledgeService.cs
@@ -0,0 +1,9 @@
+﻿using AgentMesh.Application.Models.Knowledge;
+
+namespace AgentMesh.Application.Contracts
+{
+    public interface IKnowledgeService
+    {
+        Task<KnowledgeQueryResult> QueryKnowledgeAsync(KnowledgeQuery query);
+    }
+}
---
+++ AgentMesh.Application/Models/Knowledge/KnowledgeQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace AgentMesh.Application.Models.Knowledge
+{
+    public class KnowledgeQuery
+    {
+        public string Query { get; set; } = string.Empty;
+        public IEnumerable<string> PrimaryRelevanceKeywords { get; set; } = [];
+        public IEnumerable<string> SecondaryRelevanceKeywords { get; set; } = [];
+        public int MaxResults { get; set; } = 10;
+        public bool IncludeEntities { get; set; } = true;
+        public bool IncludeRelations { get; set; } = true;
+    }
+}
