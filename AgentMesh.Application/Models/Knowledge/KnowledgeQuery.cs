70ef1522f6258176218e83f3dde5c34321808cd6
Demetrio Marra <demetrio.marra3@gmail.com>
2026-08-28 09:31:24 +0200

    KnowledgeQuery and retrieval kind

--- AgentMesh.Application/Models/Knowledge/KnowledgeQuery.cs
+++ AgentMesh.Application/Models/Knowledge/KnowledgeQuery.cs
@@ -2,7 +2,8 @@
 {
     public class KnowledgeQuery
     {
-        public string Query { get; set; } = string.Empty;
+        public string QueryText { get; set; } = string.Empty;
+        public KnowledgeQueryRetrievalKind QueryRetrievalKind { get; set; } = KnowledgeQueryRetrievalKind.SemanticAndGraph;
         public IEnumerable<string> PrimaryRelevanceKeywords { get; set; } = [];
         public IEnumerable<string> SecondaryRelevanceKeywords { get; set; } = [];
         public int MaxResults { get; set; } = 10;
